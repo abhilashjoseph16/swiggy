d0440d3a409d0372d63168260bbddc2ce15bccd8
unknown <abhilash.pannikottil@gmail.com>
2024-09-20 21:53:05 +0530

    Done

--- src/components/containers/Container.jsx
+++ src/components/containers/Container.jsx
@@ -4,24 +4,24 @@ import swiggyContainerPhoto from "../../assets/swiggy-container-photo.png"
 const Container = () => {
   return (
     <div className="main-container">
-
       <div className="first-box">
-        <div className="first-box-1">
-          <h1 className="order-food-head">
-            Order Food Online in Bangalore
-          </h1>
-          <div className="first-box-2">
+        <div className="order-food-head">
+            <h1 className="order-food">
+              Order Food Online in Bangalore
+            </h1>
+          </div>
+          <div className="order-food-underline-head">
             <svg className="order-food-underline"  viewBox="0 0 78 6" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M1 5.25939C27 -0.240624 53.5 -0.2406 77 4.25939" stroke="#FF5200" stroke-width="1.5"></path>
             </svg>
           </div>
-        </div>
       </div>
 
       <div className="second-box">
-        <img className="swiggy-container-image" src={swiggyContainerPhoto} alt="Swiggy"/>
+        <div className="siwggy-container-image-head">
+          <img className="swiggy-container-image" src={swiggyContainerPhoto} alt="Swiggy"/>
+        </div>
       </div>
-      
     </div>
   )
 }
